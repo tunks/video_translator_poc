3cd8848f541e642864f79a561f17fb6aa5e20297
ebrimatunkara <ebrimatunkara@gmail.com>
2018-08-22 12:34:52 -0400

    Initial Commit

---
+++ video_translator_poc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  video_translator_poc
+//
+//  Created by Ebrima Tunkara on 8/22/18.
+//  Copyright © 2018 Ebrima Tunkara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ video_translator_pocTests/video_translator_pocTests.swift
@@ -0,0 +1,36 @@
+//
+//  video_translator_pocTests.swift
+//  video_translator_pocTests
+//
+//  Created by Ebrima Tunkara on 8/22/18.
+//  Copyright © 2018 Ebrima Tunkara. All rights reserved.
+//
+
+import XCTest
+@testable import video_translator_poc
+
+class video_translator_pocTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
